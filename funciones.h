af28ff91e70fb380f47622d82ab8cd3b81959988
MelinaMamani <erikamelinamamani@gmail.com>
2018-04-12 22:42:05 -0300

    tp 1 primer commit

---
+++ funciones.h
@@ -0,0 +1,31 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+/** \brief solicita 2 numeros al usuario y devuelve como resultado la suma de estos
+ **/
+void menu(float, float);
+
+/** \brief Muestra la suma de los numeros ingresados
+ **/
+void suma(float, float);
+
+/** \brief Muestra la resta de los numeros ingresados
+ **/
+void resta(float, float);
+
+/** \brief Muestra la division de los numeros ingresados
+ **/
+void division(float, float);
+
+/** \brief Muestra la multiplicacion de los numeros ingresados
+ **/
+void mult(float, float);
+
+/** \brief Muestra el factorial del primer numero ingresado
+ **/
+void factorial(float);
+
+/** \brief Verifica la respuesta del usuario
+ **/
+unsigned int verifica(void);
+
+#endif
